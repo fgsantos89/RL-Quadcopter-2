6d1cfb618791fd818c426eef81dbcd86bc746f4b
Felipe Santos <santos.flip@gmail.com>
2018-08-13 22:04:38 -0300

    consolidando os graficos dos treinos

--- plots.py
+++ plots.py
@@ -3,14 +3,16 @@ import numpy as np
 
 
 def plot_training(results):
-    fig, axarr = plt.subplots(3, 2, figsize=(12, 8))
+    fig, axarr = plt.subplots(4, 2, figsize=(22, 10))
 
     reward_training(results, axarr[0][0])
-    position_training(results['runs'][0], axarr[0][1])
-    velocity_axis_training(results['runs'][0], axarr[1][0])
-    rotation_axis_training(results['runs'][0], axarr[1][1])
-    velocity_angles_training(results['runs'][0], axarr[2][0])
-    agent_choices_training(results['runs'][0], axarr[2][1])
+    position_training_x(results, axarr[0][1])
+    position_training_y(results, axarr[1][0])
+    position_training_z(results, axarr[1][1])
+    velocity_axis_training(results, axarr[2][0])
+    rotation_axis_training(results, axarr[2][1])
+    velocity_angles_training(results, axarr[3][0])
+    agent_choices_training(results, axarr[3][1])
     plt.subplots_adjust(hspace=.3)
 
 
@@ -20,49 +22,73 @@ def reward_training(results, plot):
     plot.legend()
 
 
-def position_training(results, plot):
-    plot.set_title("Positions", fontsize=18)
-    plot.plot(results['time'], results['x'], label='x')
-    plot.plot(results['time'], results['y'], label='y')
-    plot.plot(results['time'], results['z'], label='z')
-    plot.legend()
+def position_training_x(results, plot):
+    plot.set_title("Positions X", fontsize=18)
+    for run in results['runs']:
+        plot.plot(run['time'], run['x'], alpha=0.1, color='red')
+
+
+def position_training_y(results, plot):
+    plot.set_title("Positions Y", fontsize=18)
+    for run in results['runs']:
+        plot.plot(run['time'], run['y'], alpha=0.1, color='red')
+
+
+def position_training_z(results, plot):
+    plot.set_title("Positions Z", fontsize=18)
+    for run in results['runs']:
+        plot.plot(run['time'], run['z'], alpha=0.1, color='red')
 
 
 def velocity_axis_training(results, plot):
     plot.set_title("Velocity Axis", fontsize=18)
-    plot.plot(results['time'], results['x_velocity'], label='x_hat')
-    plot.plot(results['time'], results['y_velocity'], label='y_hat')
-    plot.plot(results['time'], results['z_velocity'], label='z_hat')
-    plot.legend()
+    for run in results['runs']:
+        # label='x_hat'
+        plot.plot(run['time'], run['x_velocity'], alpha=0.1, color='red')
+        # label='y_hat'
+        plot.plot(run['time'], run['y_velocity'], alpha=0.1, color='blue')
+        # label='z_hat'
+        plot.plot(run['time'], run['z_velocity'], alpha=0.1, color='green')
 
 
 def rotation_axis_training(results, plot):
     plot.set_title("Rotation Axis", fontsize=18)
-    plot.plot(results['time'], results['phi'], label='phi')
-    plot.plot(results['time'], results['theta'], label='theta')
-    plot.plot(results['time'], results['psi'], label='psi')
-    plot.legend()
+    for run in results['runs']:
+        # phi
+        plot.plot(run['time'], run['phi'], alpha=0.1, color='red')
+        # theta
+        plot.plot(run['time'], run['theta'], alpha=0.1, color='blue')
+        # psi
+        plot.plot(run['time'], run['psi'], alpha=0.1, color='green')
 
 
 def velocity_angles_training(results, plot):
     plot.set_title("Velocity of angles", fontsize=18)
-    plot.plot(results['time'], results['phi_velocity'],
-              label='phi_velocity')
-    plot.plot(results['time'], results['theta_velocity'],
-              label='theta_velocity')
-    plot.plot(results['time'], results['psi_velocity'],
-              label='psi_velocity')
-    plot.legend()
+    for run in results['runs']:
+        # label='phi_velocity'
+        plot.plot(run['time'], run['phi_velocity'],
+                  alpha=0.1, color='red')
+        # label='theta_velocity'
+        plot.plot(run['time'], run['theta_velocity'],
+                  alpha=0.1, color='blue')
+        # label='psi_velocity'
+        plot.plot(run['time'], run['psi_velocity'],
+                  alpha=0.1, color='green')
 
 
 def agent_choices_training(results, plot):
     plot.set_title("Choices", fontsize=18)
-    plot.plot(results['time'], results['rotor_speed1'],
-              label='Rotor 1 revolutions / second')
-    plot.plot(results['time'], results['rotor_speed2'],
-              label='Rotor 2 revolutions / second')
-    plot.plot(results['time'], results['rotor_speed3'],
-              label='Rotor 3 revolutions / second')
-    plot.plot(results['time'], results['rotor_speed4'],
-              label='Rotor 4 revolutions / second')
-    plot.legend()
+    for run in results['runs']:
+        plot.plot(run['time'], run['z'], alpha=0.1, color='red')
+        # label='Rotor 1 revolutions / second'
+        plot.plot(run['time'], run['rotor_speed1'],
+                  alpha=0.1, color='red')
+        # label='Rotor 2 revolutions / second'
+        plot.plot(run['time'], run['rotor_speed2'],
+                  alpha=0.1, color='blue')
+        # label='Rotor 3 revolutions / second'
+        plot.plot(run['time'], run['rotor_speed3'],
+                  alpha=0.1, color='green')
+        # label='Rotor 4 revolutions / second'
+        plot.plot(run['time'], run['rotor_speed4'],
+                  alpha=0.1, color='orange')
